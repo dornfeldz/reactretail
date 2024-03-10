22bcd2a2945eb1c43a3f02c29abbc4f605631034
lilxoli <dornfeldz@gmail.com>
2024-03-10 19:01:27 +0100

    small fixes

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -1,4 +1,4 @@
-import {useContext, useState} from "react";
+import {useContext} from "react";
 import {Context} from "./AppLayout.jsx";
 import CartItem from "./CartItem.jsx";
 import {Link} from "react-router-dom";
--- src/Header.jsx
+++ src/Header.jsx
@@ -5,10 +5,10 @@ function Header() {
     return (
         <div className="flex justify-between px-5 py-4 bg-yellow-300 mb-6">
             <Link to="/">
-                <h1 className="font-bold text-xl">E-commerce Store</h1>
+                <h1 className="font-bold text-xl">Ecommerce Store</h1>
             </Link>
             <Link to="/cart">
-                <img src="assets/imgs/shopping_cart_FILL0_wght400_GRAD0_opsz24.svg" alt="shopping cart"/>
+                <img src="https://i.imgur.com/mAaNh2a.png" alt="shopping cart"/>
             </Link>
         </div>
     );
