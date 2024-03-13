a6e70ffb55fb08f8d68e34f64b504f4fe219621b
lilxoli <dornfeldz@gmail.com>
2024-03-13 18:49:16 +0100

    Cart Badge

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -8,7 +8,7 @@ function Cart() {
     const {cartItems} = useContext(Context);
 
     return (
-        <div className="h-screen">
+        <div className="h-full min-h-screen">
             <Link to="/" className="underline underline-offset-[3px] px-5 text-blue-700">&larr; Home</Link>
             <div className="grid grid-cols-3 gap-4 mt-3">
                 {cartItems.map(item => <CartItem item={item} key={item.id}/>)}
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,14 +1,21 @@
 import {Link} from "react-router-dom";
+import {useContext} from "react";
+import {Context} from "./AppLayout.jsx";
 
 function Header() {
+    const {cartItems} = useContext(Context);
+
     return (
         <div className="flex justify-between px-5 py-4 bg-yellow-300 mb-6">
             <Link to="/">
                 <h1 className="font-bold text-xl">Ecommerce Store</h1>
             </Link>
-            <Link to="/cart">
-                <img src="https://i.imgur.com/mAaNh2a.png" alt="shopping cart" className="text-md"/>
-            </Link>
+            <div className="relative">
+                <Link to="/cart">
+                    <img src="https://i.imgur.com/mAaNh2a.png" alt="shopping cart" className=""/>
+                </Link>
+                {cartItems.length > 0 && <p className="absolute bottom-4 left-3 text-yellow-300 bg-black text-xs px-2 py-1 rounded-full font-bold">{cartItems.length}</p>}
+            </div>
         </div>
     );
 }
